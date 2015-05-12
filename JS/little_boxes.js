7a418d6531882273ded2ac54e74f65cb03ffac87
Tom Hockett <tom.hockett@gmail.com>
2015-05-11 21:44:26 -0500

    added the button

--- JS/little_boxes.js
+++ JS/little_boxes.js
@@ -1,22 +1,21 @@
 $(document).ready(function() {
 
-	var boxes_per_row = 80;
 	var page_width = 960;
-	var height_and_width_of_box = Math.round(page_width/boxes_per_row);
 
-	for (var i = 0; i < Math.pow(boxes_per_row, 2); i++) {
-		$("#container").append("<div class='pixel'></div>");
-	};
+	$("#button").click(function() {
+		boxes_per_row = prompt("How many squares per row?");
+		for (var i = 0; i < Math.pow(boxes_per_row, 2); i++) {
+			$("#container").append("<div class='pixel'></div>");
+		};
 
-	$(".pixel").height(height_and_width_of_box);
-	$(".pixel").width(height_and_width_of_box);
-
-	$("#container > div").hover(function () {
-		$(this).css("background-color", "#929292");
-	});
-
-	$("#container > div").click(function () {
 		$(".pixel").css("background-color", "#1E1E1E");
+
+		var height_and_width_of_box = Math.round(page_width/boxes_per_row);
+		$(".pixel").height(height_and_width_of_box);
+		$(".pixel").width(height_and_width_of_box);
+
+		$("#container > div").hover(function () {
+			$(this).css("background-color", "#929292");
+		});
 	});
 });
-
